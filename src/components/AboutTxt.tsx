b3c159fcde4b7b3262d4657b2768dc1e8870c7c7
sdnguyen98 <nguyens5@oregonstate.edu>
2025-04-04 02:15:54 -0700

    updated temp photo

--- src/components/AboutTxt.tsx
+++ src/components/AboutTxt.tsx
@@ -2,7 +2,7 @@ const AboutTxt = () => {
     return (
           <div id="nameTag">
             <a href="https://www.linkedin.com/in/stevenduynguyen/" target="_blank" rel="noopener noreferrer">
-                <img src="/images/templogo.jpg" width="300" className="temp logo" alt="temp logo" />
+                <img src="/images/newtemp.png" width="400" className="temp logo" alt="temp logo" />
             </a>
             <h1>Steven Nguyen</h1>
             <div id="description"><h2>Software Developer</h2></div>
