264004672cba8375574fa0bc0bb99dd52432aeb0
sdnguyen98 <nguyens5@oregonstate.edu>
2025-04-04 15:00:24 -0700

    updated fonts

--- src/components/AboutTxt.tsx
+++ src/components/AboutTxt.tsx
@@ -1,12 +1,21 @@
 const AboutTxt = () => {
-    return (
-          <div id="nameTag">
-            <a href="https://www.linkedin.com/in/stevenduynguyen/" target="_blank" rel="noopener noreferrer">
-                <img src="/images/newtemp.png" width="400" className="temp logo" alt="temp logo" />
-            </a>
-            <h1>Steven Nguyen</h1>
-            <div id="description"><h2>Software Developer</h2></div>
-          </div>
-    );
-}
+  return (
+    <>
+    <div id="nameTag">
+
+      <h1>Steven Nguyen</h1>
+      <div id="description">
+        <h2>Software Developer</h2>
+      </div>
+
+    </div>
+          <img
+          src="/images/newtemp.png"
+          width="400"
+          className="background-image"
+          alt="temp logo"
+        /></>
+  );
+};
+
 export default AboutTxt;
