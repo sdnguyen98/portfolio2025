7e2744cadaf50cb7fea4d1fa1b68e98f237c9f1b
sdnguyen98 <nguyens5@oregonstate.edu>
2025-04-04 12:52:48 -0700

    added an no scroll div for home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import Scene from "../components/Scene";
 import AboutTxt from "../components/AboutTxt";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+
 const Home = () => {
+  useEffect(() => {
+    // Add a class to the body element
+    document.body.classList.add("no-scroll");
+
+    // Cleanup: Remove the class when the component unmounts
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, []);
+
   return (
-    
     <div id="homeContainer">
       <AboutTxt />
       <Scene />
