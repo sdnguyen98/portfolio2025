c8b31f623e44087acd5ee0b2e1781285f861ec91
sdnguyen98 <nguyens5@oregonstate.edu>
2025-05-15 21:58:49 -0700

    added tailwind css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,22 +6,31 @@ import AnimatedSection from "../components/SectionAnimation";
 const About = () => {
     return (
         
-        <AnimatedSection>
-            <div className="about-container">
-                <h1 className="about-title">About</h1>
-                <div className="about-content">
-                    <div className="about-text">
-                        <p>
-                            A passionate computer science graduate from Oregon State University<br />
+        // <AnimatedSection>
+        <div className="p-8">
+        <div className="flex items-center gap-8">
+            {/* Text on the left */}
+            <div className="text-left">
+            <h2 className="text-[3.2rem] mb-4">About Me</h2>
+            <p className="text-[1.2rem] leading-[1.6]">
+                  A passionate computer science graduate from Oregon State University<br />
                             with a strong background in web development and business requirements analysis.
-                        </p>
-                    </div>
-                    <div className="about-image">
-                        <img src="https://placecats.com/400/500" alt="Placeholder Image" />
-                    </div>
-                </div>
+            </p>
             </div>
-        </AnimatedSection>
+
+            {/* Image on the right */}
+            <div className="w-40 h-40 flex-shrink-0">
+            <img
+                src="images/steven_icon.jpg"
+                alt="Placeholder Image"
+                className="w-full h-full object-cover rounded-lg"
+                
+            />
+            </div>
+        </div>
+        </div>
+
+
     );
 };
 
