1764d7ccd54ad5e993f807aa0a813de0bc383529
sdnguyen98 <nguyens5@oregonstate.edu>
2025-05-29 18:58:01 -0700

    updated on hover effect for projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -20,37 +20,26 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
-
+import React from "react";
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState<string>("");
   const [hoveredProject, setHoveredProject] = useState<string | null>(null); // Track hovered project
 
-  // Project data
-  const projectDescriptions: { [key: string]: string } = {
-    project1: "Lead a team of developers to create the new engineering website for Oregon State University",
-    project2: "The fast, easy and free tool for making digital scrapbooking layouts",
-    project3: "Application that quizzes you on your knowledge of any Spotify playlist",
-    project4: "Need to make reservation at Oregon State University Gym",
-    project5: "Want to get the .CSV of your Age of Empire 2 Analytics",
+  // State to track mouse position for each card
+  const [hoverPos, setHoverPos] = useState<{ x: number; y: number } | null>(null);
+
+  // Handler for mouse move
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const rect = (e.target as HTMLElement).getBoundingClientRect();
+    setHoverPos({
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    });
   };
 
-  const projectImages: { [key: string]: string } = {
-    project1: "../media/osu.gif",
-    project2: "https://placecats.com/300/200",
-    project3: "https://placecats.com/300/200",
-    project4: "../media/benny.gif",
-    project5: "../media/villager.gif",
-  };
-
-  // Project names
-  const projectNames: { [key: string]: string } = {
-    project1: "Oregon State University College of Engineering",
-    project2: "Scrapbook Maker",
-    project3: "Spotify Quiz",
-    project4: "Gym Reservation",
-    project5: "Age of Empires Analytics Scrapper",
-  };
+  // Handler for mouse leave
+  const handleMouseLeave = () => setHoverPos(null);
 
   return (
     <div className="projects-container">
@@ -60,7 +49,11 @@ const Projects = () => {
         <AccordionItem value="item-1">
           <Card>
             <HoverCard>
-              <HoverCardTrigger>
+              <HoverCardTrigger
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                style={{ width: "100%" }}
+              >
                 <CardContent>
                   <AccordionTrigger><h2>Oregon State University College of Engineering Websites</h2></AccordionTrigger>
                   <AccordionContent className="text-left relative">
@@ -71,7 +64,20 @@ const Projects = () => {
                   </AccordionContent>
                 </CardContent>
               </HoverCardTrigger>
-              <HoverCardContent>
+              <HoverCardContent
+                style={
+                  hoverPos
+                    ? {
+                        position: "absolute",
+                        left: hoverPos.x,
+                        top: hoverPos.y,
+                        pointerEvents: "none",
+                        zIndex: 1000,
+                        transform: "translate(-150%, -100%)",
+                      }
+                    : { display: "none" }
+                }
+              >
               <img
                         src="../media/osu.gif"
                         alt="Placeholder Image"
@@ -84,38 +90,154 @@ const Projects = () => {
         </AccordionItem>
         <AccordionItem value="item-2">
           <Card>
+          <HoverCard>
+          <HoverCardTrigger
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{ width: "100%" }}
+          >
             <CardContent>
               <AccordionTrigger>Scrapbook Maker</AccordionTrigger>
               <AccordionContent className="text-left">The fast, easy and free tool for making digital scrapbooking layouts
               </AccordionContent>
             </CardContent>
+            </HoverCardTrigger>
+              <HoverCardContent
+                style={
+                  hoverPos
+                    ? {
+                        position: "absolute",
+                        left: hoverPos.x,
+                        top: hoverPos.y,
+                        pointerEvents: "none",
+                        zIndex: 1000,
+                        transform: "translate(-150%, -100%)",
+                      }
+                    : { display: "none" }
+                }
+              >
+              <img
+                        src="../media/villager.gif"
+                        alt="Placeholder Image"
+                        className="w-full h-full object-cover rounded-lg"
+
+                    />
+              </HoverCardContent>
+            </HoverCard>
           </Card>
         </AccordionItem>
         <AccordionItem value="item-3">
           <Card>
+          <HoverCard>
+          <HoverCardTrigger
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{ width: "100%" }}
+          >
             <CardContent>
               <AccordionTrigger>Spotify Quiz</AccordionTrigger>
               <AccordionContent className="text-left">Application that quizzes you on your knowledge of any Spotify playlist
               </AccordionContent>
             </CardContent>
+            </HoverCardTrigger>
+              <HoverCardContent
+                style={
+                  hoverPos
+                    ? {
+                        position: "absolute",
+                        left: hoverPos.x,
+                        top: hoverPos.y,
+                        pointerEvents: "none",
+                        zIndex: 1000,
+                        transform: "translate(-150%, -100%)",
+                      }
+                    : { display: "none" }
+                }
+              >
+              <img
+                        src="../media/villager.gif"
+                        alt="Placeholder Image"
+                        className="w-full h-full object-cover rounded-lg"
+
+                    />
+              </HoverCardContent>
+            </HoverCard>
           </Card>
         </AccordionItem>
         <AccordionItem value="item-4">
           <Card>
+          <HoverCard>
+          <HoverCardTrigger
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{ width: "100%" }}
+          >
             <CardContent>
               <AccordionTrigger>Gym Reservation</AccordionTrigger>
               <AccordionContent className="text-left">Need to make reservation at Oregon State University Gym
               </AccordionContent>
             </CardContent>
+            </HoverCardTrigger>
+              <HoverCardContent
+                style={
+                  hoverPos
+                    ? {
+                        position: "absolute",
+                        left: hoverPos.x,
+                        top: hoverPos.y,
+                        pointerEvents: "none",
+                        zIndex: 1000,
+                        transform: "translate(-150%, -100%)",
+                      }
+                    : { display: "none" }
+                }
+              >
+              <img
+                        src="../media/villager.gif"
+                        alt="Placeholder Image"
+                        className="w-full h-full object-cover rounded-lg"
+
+                    />
+              </HoverCardContent>
+            </HoverCard>
           </Card>
         </AccordionItem>
         <AccordionItem value="item-5">
           <Card>
+          <HoverCard>
+          <HoverCardTrigger
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{ width: "100%" }}
+          >
             <CardContent>
               <AccordionTrigger>Age of Empires Analytics Scrapper</AccordionTrigger>
               <AccordionContent className="text-left">Want to get the .CSV of your Age of Empire 2 Analytics
               </AccordionContent>
             </CardContent>
+            </HoverCardTrigger>
+              <HoverCardContent
+                style={
+                  hoverPos
+                    ? {
+                        position: "absolute",
+                        left: hoverPos.x,
+                        top: hoverPos.y,
+                        pointerEvents: "none",
+                        zIndex: 1000,
+                        transform: "translate(-150%, -100%)",
+                      }
+                    : { display: "none" }
+                }
+              >
+              <img
+                        src="../media/villager.gif"
+                        alt="Placeholder Image"
+                        className="w-full h-full object-cover rounded-lg"
+
+                    />
+              </HoverCardContent>
+            </HoverCard>
           </Card>
         </AccordionItem>
       </Accordion>
